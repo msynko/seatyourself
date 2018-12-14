382a8b63c9852e98dde10f74a1534a94183636c2
victoriak94 <vkamila6@gmail.com>
2018-12-14 15:46:36 -0500

    Render edit form on restaurant edit view. Add links to edit, delete, and back to restaurant/s.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,21 @@
 class ReservationsController < ApplicationController
 
   def index
-
+    @reservations = Reservation.all
   end
 
-  def
+  def new
+    @reservations = Reservation.new
   end
-  
+
+  def show
+    @reservations = Reservation.find(params[:id])
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to "/reservations", notice: "Reservation deleted"
+  end
+
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,35 +6,33 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
-
   end
 
   def new
     @restaurant = Restaurant.new
-end
+  end
 
   def create
-  @restaurant = Restaurant.new
-  @restaurant.name = params[:restaurant][:name]
-  @restaurant.address = params[:restaurant][:address]
-  @restaurant.phone_number = params[:restaurant][:phone_number]
-  @restaurant.price_range = params[:restaurant][:price_range]
-  @restaurant.max_capacity = params[:restaurant][:max_capacity]
-  @restaurant.opening_time = params[:restaurant][:opening_time]
-  @restaurant.closing_time = params[:restaurant][:closing_time]
-  @restaurant.menu = params[:restaurant][:menu]
+    @restaurant = Restaurant.new
+    @restaurant.name = params[:restaurant][:name]
+    @restaurant.address = params[:restaurant][:address]
+    @restaurant.phone_number = params[:restaurant][:phone_number]
+    @restaurant.price_range = params[:restaurant][:price_range]
+    @restaurant.max_capacity = params[:restaurant][:max_capacity]
+    @restaurant.opening_time = params[:restaurant][:opening_time]
+    @restaurant.closing_time = params[:restaurant][:closing_time]
+    @restaurant.menu = params[:restaurant][:menu]
 
-  if @restaurant.save
-    redirect_to product_path(@restaurant), notice: "You have added your restaurant!"
-  else
-    render :new, notice: "We could not add your restaurant"
-
+    if @restaurant.save
+      redirect_to product_path(@restaurant), notice: "You have added your restaurant!"
+    else
+      render :new, notice: "We could not add your restaurant"
+    end
   end
-end
 
   def edit
     @restaurant = Restaurant.find(params[:id])
-end
+  end
 
   def update
     @restaurant = Restaurant.find(params[:id])
@@ -46,19 +44,18 @@ end
     @restaurant.opening_time = params[:restaurant][:opening_time]
     @restaurant.closing_time = params[:restaurant][:closing_time]
     @restaurant.menu = params[:restaurant][:menu]
+
     if @restaurant.save
       redirect_to product_path(@restaurant), notice: "You have added your restaurant!"
     else
       render :new, notice: "We could not add your restaurant"
-
     end
-
-end
+  end
 
   def destroy
-  @restaurant= Restaurant.find(params[:id])
-  @restaurant.destroy
-  redirect_to "/restaurants", notice: "Restaurant deleted"
-end
+    @restaurant= Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to "/restaurants", notice: "Restaurant deleted"
+  end
 
 end
