807168e434f48c5a211b5f0cef670422d2b268d2
Gabriela <gab.jo.cuello@gmail.com>
2018-12-14 15:17:07 -0500

    Add create, edit, update,destroy methods in restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,15 +14,51 @@ class RestaurantsController < ApplicationController
 end
 
   def create
+  @restaurant = Restaurant.new
+  @restaurant.name = params[:restaurant][:name]
+  @restaurant.address = params[:restaurant][:address]
+  @restaurant.phone_number = params[:restaurant][:phone_number]
+  @restaurant.price_range = params[:restaurant][:price_range]
+  @restaurant.max_capacity = params[:restaurant][:max_capacity]
+  @restaurant.opening_time = params[:restaurant][:opening_time]
+  @restaurant.closing_time = params[:restaurant][:closing_time]
+  @restaurant.menu = params[:restaurant][:menu]
+
+  if @restaurant.save
+    redirect_to product_path(@restaurant), notice: "You have added your restaurant!"
+  else
+    render :new, notice: "We could not add your restaurant"
+
+  end
 end
 
   def edit
+    @restaurant = Restaurant.find(params[:id])
 end
 
   def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.name = params[:restaurant][:name]
+    @restaurant.address = params[:restaurant][:address]
+    @restaurant.phone_number = params[:restaurant][:phone_number]
+    @restaurant.price_range = params[:restaurant][:price_range]
+    @restaurant.max_capacity = params[:restaurant][:max_capacity]
+    @restaurant.opening_time = params[:restaurant][:opening_time]
+    @restaurant.closing_time = params[:restaurant][:closing_time]
+    @restaurant.menu = params[:restaurant][:menu]
+    if @restaurant.save
+      redirect_to product_path(@restaurant), notice: "You have added your restaurant!"
+    else
+      render :new, notice: "We could not add your restaurant"
+
+    end
+
 end
 
   def destroy
+  @restaurant= Restaurant.find(params[:id])
+  @restaurant.destroy
+  redirect_to "/restaurants", notice: "Restaurant deleted"
 end
 
 end
