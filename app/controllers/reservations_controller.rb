318c64459186546c98768e026b72b2ea699ff809
Gabriela <gab.jo.cuello@gmail.com>
2018-12-14 17:50:00 -0500

    fix create method for reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,13 +9,17 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    @reservation = Reservation.new
+    @reservation.time = params[:reservation][:time]
+    @reservation.party_size = params[:reservation][:party_size]
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reservation.restaurant= @restaurant
     if @reservation.save
-      redirect_to product_path(@restaurant), notice: "You have added your restaurant!"
+      redirect_to restaurant_path(@reservation), notice: "You have added your restaurant!"
     else
       render :new, notice: "We could not add your restaurant"
     end
   end
-
 
   def show
     @reservations = Reservation.find(params[:id])
