2d256b005e1d56221faba1fa3708fc7ae9ee98b5
victoriak94 <vkamila6@gmail.com>
2018-12-14 15:30:20 -0500

    Fixed indentations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,20 +1,10 @@
 class ReservationsController < ApplicationController
 
   def index
-    @reservations = Reservation.all
 
   end
 
-  def new
-    @reservations = Reservation.new
-end
-
-def show
-  @reservations = Reservation.find(params[:id])
-end
-
-def destroy
-  @reservation = Reservation.find(params[:id])
-  @reservation.destroy
-  redirect_to "/reservations", notice: "Reservation deleted"
+  def
+  end
+  
 end
