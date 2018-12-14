4d3b43107c35880de751c07f19e72db45600c309
victoriak94 <vkamila6@gmail.com>
2018-12-14 16:51:46 -0500

    Create current capacity method for a reservation validation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,6 +8,15 @@ class ReservationsController < ApplicationController
     @reservations = Reservation.new
   end
 
+  def create
+    if @reservation.save
+      redirect_to product_path(@restaurant), notice: "You have added your restaurant!"
+    else
+      render :new, notice: "We could not add your restaurant"
+    end
+  end
+
+
   def show
     @reservations = Reservation.find(params[:id])
   end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,21 @@
 class Reservation < ApplicationRecord
 
+  validate :current_capacity
+
   belongs_to :user
   belongs_to :restaurant
 
+  def current_capacity
+    restaurant_capacity = restaurant.max_capacity
+    current_reservations = restaurant.reservations.where("time = ?", self.time)
+    total_current_occupants = 0
+    current_reservations.each do |reservation|
+      total_current_occupants += reservation.party_size
+    end
+
+    if restaurant.max_capacity < total_current_occupants
+      errors[:base] << "Reservation not booked. Restaurant at capacity."
+      #reservation booking not possible
+    end
+  end
 end
