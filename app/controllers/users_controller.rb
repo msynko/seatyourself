bfbd365f410f4cc5c651d2ca666b2d3a7aa53cbb
Gabriela <gab.jo.cuello@gmail.com>
2018-12-16 13:09:39 -0500

    Create sign up form for users with password digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.phone_number = params[:user][:phone_number]
+
+    if @user.save
+      redirect_to root_url
+      flash[:notice] = "You have created your account!"
+    else
+      render :new
+      flash[:notice] = "We could not create your account"
+    end
+  end
+
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user= User.find(params[:id])
+    @user.destroy
+    redirect_to "/restaurants"
+    flash[:notice] = "Account deleted"
+  end
+
 end
