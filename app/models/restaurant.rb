3603868b3bf57c1456dad414acad36e4cfb86742
msynko <marina.synko@gmail.com>
2018-12-17 20:54:40 -0500

    style, li, edit, delete links

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,16 @@ class Restaurant < ApplicationRecord
 
   has_many :reservations
   belongs_to :user
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :price_range, presence: true
+  validates :opening_time, presence: true
+  validates :closing_time, presence: true
+  validates :max_capacity, presence: true
+  validates :menu, presence: true
+
+
 
   def closing_time_display
     if closing_time > 12
