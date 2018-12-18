16531f10fdcd248f4929ee5864cf31cbbfad404f
victoriak94 <vkamila6@gmail.com>
2018-12-18 12:07:10 -0500

    Edit stylesheets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   def ensure_logged_in
     unless current_user
       flash[:alert] = "Please log in"
-      redirect_to new_session_url
+      redirect_to new_sessions_url
     end
   end
 
